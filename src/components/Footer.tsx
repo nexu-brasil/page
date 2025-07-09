5facb4c8a2ab8d734ad5f8581c7050f065c7e141
alskdkkk <felipeluizharrotte@gmail.com>
2025-07-09 10:26:08 -0300

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
           {/* Logo and description */}
           <div className="md:col-span-2">
             <img 
-              src="public/images/nexu-bg.png" 
+              src="/images/nexu-bg.png" 
               alt="Nexu Assessoria"
               className="h-12 w-auto mb-6"
             />
